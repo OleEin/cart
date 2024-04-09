2bb5991ab7f2fd8231b3dd8b7d65ea84c66345ae
OleEin <104646763+OleEin@users.noreply.github.com>
2024-04-09 17:12:15 +0200

    Add Bcrypt JS

--- scripts/seed.js
+++ scripts/seed.js
@@ -5,8 +5,7 @@ const {
   revenue,
   users,
 } = require('../app/lib/placeholder-data.js');
-const bcrypt = require('bcrypt');
-
+const bcrypt = require('bcryptjs');
 async function seedUsers(client) {
   try {
     await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
