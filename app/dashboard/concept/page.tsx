d280c32d59900f08735c73a9c1518b4d8d1ae8c6
OleEin <oe@xocore.de>
2024-04-17 10:51:33 +0200

    Class / ClassName

--- app/dashboard/concept/page.tsx
+++ app/dashboard/concept/page.tsx
@@ -107,18 +107,18 @@ export default function Example() {
       </div>
     </div>
 
-    <fieldset class="space-y-4">
-  <legend class="sr-only">Delivery</legend>
+    <fieldset className="space-y-4">
+  <legend className="sr-only">Delivery</legend>
 
   <div>
     <label
       for="DeliveryStandard"
-      class="flex cursor-pointer justify-between gap-4 rounded-lg border border-gray-100 bg-white p-4 text-sm font-medium shadow-sm hover:border-gray-200 has-[:checked]:border-blue-500 has-[:checked]:ring-1 has-[:checked]:ring-blue-500"
+      className="flex cursor-pointer justify-between gap-4 rounded-lg border border-gray-100 bg-white p-4 text-sm font-medium shadow-sm hover:border-gray-200 has-[:checked]:border-blue-500 has-[:checked]:ring-1 has-[:checked]:ring-blue-500"
     >
       <div>
-        <p class="text-gray-700">Standard</p>
+        <p className="text-gray-700">Standard</p>
 
-        <p class="mt-1 text-gray-900">Free</p>
+        <p className="mt-1 text-gray-900">Free</p>
       </div>
 
       <input
@@ -126,7 +126,7 @@ export default function Example() {
         name="DeliveryOption"
         value="DeliveryStandard"
         id="DeliveryStandard"
-        class="size-5 border-gray-300 text-blue-500"
+        className="size-5 border-gray-300 text-blue-500"
         checked
       />
     </label>
@@ -135,12 +135,12 @@ export default function Example() {
   <div>
     <label
       for="DeliveryPriority"
-      class="flex cursor-pointer justify-between gap-4 rounded-lg border border-gray-100 bg-white p-4 text-sm font-medium shadow-sm hover:border-gray-200 has-[:checked]:border-blue-500 has-[:checked]:ring-1 has-[:checked]:ring-blue-500"
+      className="flex cursor-pointer justify-between gap-4 rounded-lg border border-gray-100 bg-white p-4 text-sm font-medium shadow-sm hover:border-gray-200 has-[:checked]:border-blue-500 has-[:checked]:ring-1 has-[:checked]:ring-blue-500"
     >
       <div>
-        <p class="text-gray-700">Next Day</p>
+        <p className="text-gray-700">Next Day</p>
 
-        <p class="mt-1 text-gray-900">£9.99</p>
+        <p className="mt-1 text-gray-900">£9.99</p>
       </div>
 
       <input
@@ -148,7 +148,7 @@ export default function Example() {
         name="DeliveryOption"
         value="DeliveryPriority"
         id="DeliveryPriority"
-        class="size-5 border-gray-300 text-blue-500"
+        className="size-5 border-gray-300 text-blue-500"
       />
     </label>
   </div>
