9ea795b891c33abc535b3f6440917f07b26b3762
OleEin <oe@xocore.de>
2024-04-17 10:43:14 +0200

    Correction of Spelling

--- app/dashboard/person/page.tsx
+++ app/dashboard/person/page.tsx
@@ -69,7 +69,7 @@ export default function Page() {
           </p>
           <Link href="/dashboard/overview">
             <button className="bg-blue-500 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded mt-4">
-              ---> Weiter
+               Weiter
             </button>
           </Link>
         </div>
