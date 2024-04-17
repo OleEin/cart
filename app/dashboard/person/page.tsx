faf90c8e5d9e8d3eb557f1ce8238f38756aa458b
OleEin <oe@xocore.de>
2024-04-17 10:27:42 +0200

    Stage Personal

--- app/dashboard/person/page.tsx
+++ app/dashboard/person/page.tsx
@@ -1,3 +1,81 @@
+'use client'
+
+import { CheckIcon } from '@heroicons/react/20/solid'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
+import { useState, useEffect  } from 'react';
+import Link from 'next/link';
+
 export default function Page() {
-    return <p>Dashboard Page</p>;
-  }
+  const includedFeatures = [
+    'Private forum access',
+    'Member resources',
+    'Entry to annual conference',
+    'Official member t-shirt',
+  ]
+
+    // State-Variablen für die Slider-Werte und die Summe der Slider-Werte
+    const initialPersonal = JSON.parse(localStorage.getItem('personal')) || { leadersValue: 50, employeesValue: 50 };
+    const [leadersValue, setLeadersValue] = useState(initialPersonal.leadersValue);
+    const [employeesValue, setEmployeesValue] = useState(initialPersonal.employeesValue);
+  
+    // Funktion zum Berechnen der Summe der Slider-Werte
+    const totalPersons = leadersValue + employeesValue;
+
+    useEffect(() => {
+      localStorage.setItem('personal', JSON.stringify({ leadersValue, employeesValue }));
+    }, [leadersValue, employeesValue]);
+
+    return (
+      <>
+   <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl sm:text-center mb-8">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            Wie viele Mitarbeiter sind Teil der Maßnahme?
+          </h2>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+            Distinctio et nulla eum soluta et neque labore quibusdam. Saepe et quasi iusto modi velit ut non voluptas
+            in. Explicabo id ut laborum.
+          </p>
+        </div>
+
+        <div className="mx-auto max-w-2xl flex flex-col gap-8">
+          <div className="flex flex-col items-center justify-center">
+            <h3 className="text-xl font-semibold mb-4">Führungskräfte</h3>
+            <Box sx={{ width: '80%' }}>
+              <Slider
+                defaultValue={initialPersonal.leadersValue}
+                aria-label="Führungskräfte"
+                valueLabelDisplay="auto"
+                onChange={(event, value) => setLeadersValue(value as number)}
+              />
+            </Box>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <h3 className="text-xl font-semibold mb-4">Mitarbeiter</h3>
+            <Box sx={{ width: '80%' }}>
+              <Slider
+                defaultValue={initialPersonal.employeesValue}
+                aria-label="Mitarbeiter"
+                valueLabelDisplay="auto"
+                onChange={(event, value) => setEmployeesValue(value as number)}
+              />
+            </Box>
+          </div>
+          <p className="text-lg text-gray-600">
+            Die geplante Maßnahme bezieht sich auf insgesamt {totalPersons} Personen.
+          </p>
+          <Link href="/dashboard/overview">
+            <button className="bg-blue-500 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded mt-4">
+              ---> Weiter
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  
+  </>
+)
+}
